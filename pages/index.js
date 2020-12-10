1757570f2c48ca5f42959dc6d9857bc786f876e4
triinperkson <75750925+triinperkson@users.noreply.github.com>
2020-12-10 09:40:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -176,9 +176,9 @@ export default function Home() {
                       "dd.MM.yyyy"
                     )}
                   </span>
-                
+                <br />
  <span className="text-gray-500">[ Kui haigusnähte pole ilmnenud, lõpetatakse isolatsioon 10 päeva möödumisel alates positiivsest testist. Testi tegemise päeva loetakse päevaks 0. ]</span>
-                  <br /> <br>
+                  <br /> 
                </p> <p>
                   * Isolatsiooni võib lõpetada ainult siis, kui sümptomeid ei
                   ole tekkinud.{" "}
